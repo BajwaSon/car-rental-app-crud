20ad4f2675bb95506c94277525e090577e751f68
Bajwa Mandeep <pmandeepsingh365@gmail.com>
2025-12-09 10:12:17 +0400

    Updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('employee-leave-management');
+  protected readonly title = signal('car-rental-app');
 }
